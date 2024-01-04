3abf00eeb02f08c61c3f4fc4451a057faa703144
Kirill Zorin <thesecretsoftheuniverse@mail.ru>
2024-01-04 14:01:24 +0300

    upd logger, add colors

--- src/pkg/logger/logger.go
+++ src/pkg/logger/logger.go
@@ -12,11 +12,11 @@ const (
 
 func SetUpLogger(env string) *slog.Logger {
 	var log *slog.Logger
-	// todo make logger colorful
+
 	switch env {
 	case envLocal:
 		log = slog.New(
-			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+			newHandler(&slog.HandlerOptions{Level: slog.LevelDebug}),
 		)
 	case envProd:
 		log = slog.New(
